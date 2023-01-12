01f33794894f2a59dc1a5e931f829a049c789091
Kevyn Marciano <kevynmarciano426@gmail.com>
2023-01-12 16:59:12 -0300

    ajuste no js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* 
+script Anterior
 const botoes = document.querySelectorAll(".btn");
 
 botoes.forEach(function (botao) {
@@ -10,18 +12,23 @@ botoes.forEach(function (botao) {
 function off() {
   document.querySelector("#tela").innerHTML = "";
 }
+*/
+function insert(num) {
+  var numero = document.querySelector("#tela").innerHTML;
+  document.querySelector("#tela").innerHTML = numero + num;
+}
+function clean() {
+  document.querySelector("#tela").innerHTML = "";
+}
+function on() {
+  document.querySelector("#tela").innerHTML = "--------ON-------";
+}
 
-/*const botoes = document.querySelectorAll(".botao-inferior");
-
-
-botoes.forEach(function(botao){
-    let valor = botao.value;
-    botao.addEventListener("click", function(){
-        document.querySelector("#tela").innerHTML+=valor;
-    })
-
-})
-
-function limpar(){
-    document.querySelector("#tela").innerHTML="";
-}*/
+function calcular() {
+  var resultado = document.querySelector("#tela").innerHTML;
+  if (resultado) {
+    document.querySelector("#tela").innerHTML = eval(resultado);
+  } else {
+    document.querySelector("#tela").innerHTML = "Nada...";
+  }
+}
